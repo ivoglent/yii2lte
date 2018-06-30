0d885110e844843f3afccc6ab09424a032ed2791
Long Nguyen <ivoglent@gmail.com>
2018-06-30 15:50:56 +0700

    Change the way to set LTE components

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -34,7 +34,7 @@ if (\Yii::$app->user->isGuest === false) {
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?=$this->title?></title>
+    <title><?=!empty($this->title) ? $this->title : \Yii::t('app', 'Administrator Dashboard')?></title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <?php $this->head()?>
